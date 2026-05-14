acc6a2712e412f3ac0f615e0e53fa70a6f73c882
Nhu <256359060+ttnluong@users.noreply.github.com>
2026-05-14 23:58:47 +0200

    Add JS function show sidebar togglebutton

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-// // Truncate text, ellipsis after whole word
+// Truncate text, ellipsis after whole word
 // document.addEventListener("DOMContentLoaded", () => {
 // var text = document.querySelectorAll(".truncate").textContent;
 // var text_wrapped = text.replace(/\w+/g, '<span>$&</span>'); //$& = placeholder matched text
 
-// // Printing the result
+// Printing the result
 // document.querySelectorAll(".truncate").innerHTML = text_wrapped;
 // });
 
+const text = document.querySelectorAll(".truncate");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".truncate");
-  console.log("Found elements:", elements);
+text.forEach(p => {
+  p.innerHTML = p.textContent.replace(/\w+/g, '<span>$&</span>');
+ });
 
-  elements.forEach(p => {
-    console.log("Text:", p.textContent);
-    p.innerHTML = p.textContent.replace(/\w+/g, '<span>$&</span>');
-    console.log("New HTML:", p.innerHTML);
-  });
-});
+
+ // sidebar toggle button
+
+//  function openSidebar() {
+//   document.getElementById("sidebar").style.marginLeft = "0px";
+// }
+
+// function closeSidebar() {
+//   document.getElementById("sidebar").style.display = "none";
+// }
+
+// function openSidebar() {
+//   var element = document.getElementById("sidebar");
+//   element.classList.add("open");
+// }
+
+const toggleButton = document.querySelector('#sidebar-toggle');
+
+toggleButton.addEventListener('click', () => {
+  var el = document.getElementById("sidebar");
+  el.style.marginLeft = "0px";
+})
+
+document.body.addEventListener('click', () => {
+  document.getElementById("sidebar").style.marginLeft = "-200px";
+})
+
+
