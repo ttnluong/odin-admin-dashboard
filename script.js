2107f8787a0f7d45a112cd31002f458658d71d70
Nhu <256359060+ttnluong@users.noreply.github.com>
2026-05-15 17:00:58 +0200

    Edit HTML project descriptions, tags and buttons

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 // Truncate text, ellipsis after whole word
-// document.addEventListener("DOMContentLoaded", () => {
-// var text = document.querySelectorAll(".truncate").textContent;
-// var text_wrapped = text.replace(/\w+/g, '<span>$&</span>'); //$& = placeholder matched text
-
-// Printing the result
-// document.querySelectorAll(".truncate").innerHTML = text_wrapped;
-// });
 
 const text = document.querySelectorAll(".truncate");
-const textheading = document.querySelectorAll(".truncate.activity-heading");
+const textHeading = document.querySelectorAll(".truncate.activity-heading");
 
 text.forEach(p => {
   p.innerHTML = p.textContent.replace(/[a-z0-9_]+/g, '<span>$&</span>');
  });
 
-textheading.forEach(p => {
+textHeading.forEach(p => {
   p.innerHTML = p.textContent.replace(/\b[a-z]*[A-Z][a-zA-Z0-9_]*\b/g, '<strong><span>$&</span></strong>');
 });
 
 
- // sidebar toggle button
+ // Sidebar toggle button
 
 const toggleButton = document.getElementById("sidebar-toggle");
 const sidebar = document.querySelector(".sidebar-nav");
